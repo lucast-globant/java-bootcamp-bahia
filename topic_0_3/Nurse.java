52de082fe04ce10b68e0ccf1ce9b7db047980cdb
marcrodrig <marcelo.prf9@gmail.com>
2015-02-10 19:33:09 -0300

    Create Nurse.java

---
+++ topic_0_3/Nurse.java
@@ -0,0 +1,22 @@
+package topic_0_3;
+
+public class Nurse {
+	private String name;
+	private int dni;
+	public Nurse(String name, int dni) {
+		this.name = name;
+		this.dni = dni;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+}
