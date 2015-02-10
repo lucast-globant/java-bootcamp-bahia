07a4a615aecd14598f0a526836a2cc33e2a7136c
marcrodrig <marcelo.prf9@gmail.com>
2015-02-10 19:11:39 -0300

    Create HouseBuilder.java

---
+++ topic_0_2/HouseBuilder.java
@@ -0,0 +1,9 @@
+package topic_0_2;
+
+public interface HouseBuilder {
+	public void buildRooms();
+	public void buildDoors();
+	public void buildWindows();
+	public void buildGarage();
+	public House getHouse();
+}
