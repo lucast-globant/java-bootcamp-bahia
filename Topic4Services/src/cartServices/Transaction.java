910dddddbaa5b60b5a8d01672bd51cd5a886e89e
Mati <Mati@MATIPC>
2015-02-26 10:32:49 -0300

    Topic4 - commit without topic1

---
+++ Topic4Services/src/cartServices/Transaction.java
@@ -0,0 +1,40 @@
+package cartServices;
+
+import notifications.MailListManager;
+import payments.Payment;
+
+/* Represents a transaction :
+ *  0. Receives a full cart from customer.
+ *  1. Chose the form of payment.
+ *  2. Apply the correspondent discount.
+ *  3. Make payment.                             */
+
+public class Transaction {
+
+	private Cart cart;
+	private static int ID = 0;
+	private Payment paymentMethod;
+
+	public Transaction(Cart cart, Payment paymentMethod) {
+
+		this.cart = cart;
+		ID = Counter.getInstance().getUniqueId();
+		this.paymentMethod = paymentMethod;
+
+	}
+
+	public double Purchase() {
+
+		float total = cart.totalPrice();
+
+		paymentMethod.pay(total);
+
+		double totalDiscount = paymentMethod.getDiscount().applyDiscount(cart.getItems(), total);
+
+		MailListManager.getInstance().setMessage(" New Transaction was made ID: " + Transaction.ID);
+
+		return totalDiscount;
+
+	}
+
+}
---
+++ Topic4Services/src/notifications/MarketManager.java
@@ -0,0 +1,22 @@
+package notifications;
+
+
+public class MarketManager implements MarketManagerObserver {
+
+	String name;
+	String branchName;
+
+	public MarketManager(String name, String branchName) {
+		super();
+		this.name = name;
+		this.branchName = branchName;
+	}
+
+	@Override
+	public void email(String msg) {
+
+		System.out.println(name + " of " + branchName + " INBOX ----- MESSAGE : " + msg);
+
+	}
+
+}
