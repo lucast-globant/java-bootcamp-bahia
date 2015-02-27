2a7595358b16270e64cca01978b9e584a2935265
Santiago <santiagolirio@hotmail.com>
2015-02-26 22:34:34 -0300

    Topic0-ej3Corrected

---
+++ Topic0-ej3-corrected/hospital/floortypes/IntermediateCare.java
@@ -0,0 +1,13 @@
+package hospital.floortypes;
+
+import hospital.Floor;
+
+public class IntermediateCare implements Floor {
+
+	   private static final String INTERMEDIATE_CARE = null;
+
+	@Override
+	   public String floortype() {
+		   return INTERMEDIATE_CARE;
+	   }
+}
