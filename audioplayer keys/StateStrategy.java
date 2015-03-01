92f057130eeee175a705d949ef8303670e25864a
victoria <vicky_864@hotmail.com>
2015-03-01 19:38:22 -0300

    re do keys

--- audioplayer keys/StateStrategy.java
+++
@@ -1,7 +0,0 @@
-package topic1.audioplayer;
-
-public interface StateStrategy {
-	
-	public void executeAction (AudioPlayer player); 
-
-}
--- shoppingcart keys/notifications/Observer.java
+++
@@ -1,6 +0,0 @@
-package topic1.notifications;
-
-public interface Observer {
-
-	public void update(Mail mail);
-}
